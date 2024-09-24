1beb28d4b9af566ddfb5d6f55a35c9999b501bdd
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2024-09-24 18:33:37 -0300

    feat: configuring fire base

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFnycT4EQl8Hjy_howp__nnx2sA-UiQYI",
+  authDomain: "chat-web-84675.firebaseapp.com",
+  projectId: "chat-web-84675",
+  storageBucket: "chat-web-84675.appspot.com",
+  messagingSenderId: "103738977718",
+  appId: "1:103738977718:web:8b9fa49135eb834ea2e517",
+  measurementId: "G-G1EQSX724F",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const databaseApp = getFirestore(app);
