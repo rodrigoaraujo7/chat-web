e881d2d3f8cd22dcdda47f0a19863e0fc7cfbd5b
Rodrigo Leite Araujo <dev.rodrigoaraujo@gmail.com>
2024-09-24 18:56:50 -0300

    feat: configuring styled components GlobalStyles

---
+++ src/globalStyles.ts
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+
+    font-family: "Roboto", sans-serif;
+  }
+  
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  svg {
+    flex-shrink: 0;
+  }
+
+  body {
+    position: relative;
+  }
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from 'globalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
